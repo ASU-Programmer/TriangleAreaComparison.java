9d006de74ecc6e8bfc50a76871ecc592d74db63c
Tyler H <45977934+PVCC-Coder-py@users.noreply.github.com>
2021-09-12 22:47:00 -0700

    Create TriangleArea.java

---
+++ TriangleArea.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class TriangleArea {
+   public static void main(String[] args) {
+      Scanner scnr = new Scanner(System.in);
+      
+      Triangle triangle1 = new Triangle();
+      Triangle triangle2 = new Triangle();
+      double base1 = scnr.nextDouble();
+      double height1 = scnr.nextDouble();
+      double base2 = scnr.nextDouble();
+      double height2 = scnr.nextDouble();
+      
+      
+      // TODO: Read and set base and height for triangle1 (use setBase() and setHeight())
+      triangle1.setBase(base1);
+      triangle1.setHeight(height1);
+      // TODO: Read and set base and height for triangle2 (use setBase() and setHeight())
+      triangle2.setBase(base2);
+      triangle2.setHeight(height2);
+      // TODO: Determine larger triangle (use getArea())
+      
+      System.out.println("Triangle with larger area:");
+      // TODO: Output larger triangle's info (use printInfo())
+      if (triangle1.getArea() > triangle2.getArea()) {
+         triangle1.printInfo();
+      }
+      else {
+         triangle2.printInfo();
+      }
+   }
+}
